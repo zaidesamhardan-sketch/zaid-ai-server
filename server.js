9904ceb7173e5b2527e6b89b7e8c9121455c0e61
zaidesamhardan-sketch <zaidesamhardan@gmail.com>
2026-02-27 22:22:14 +0300

    server.js

--- server.js
+++ server.js
@@ -34,4 +34,6 @@ app.post("/chat", async (req, res) => {
   res.json({ reply: data.choices[0].message.content });
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
